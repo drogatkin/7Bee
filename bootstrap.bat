af018bcc42dd7ba54a6995bb295658ef94dadb5b
drogatkin <metricstream@gmail.com>
2013-02-23 00:30:30 -0800

    jdk path updated

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,8 +1,8 @@
 @echo off
 rem $Id: bootstrap.bat,v 1.7 2012/04/17 22:04:26 dmitriy Exp $
-set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_03
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_15
 set JAVA_DEPENDENCY=.\3rdparty\jdepend
-set JSCH=C:\tools\libs\jsch-0.1.47\jsch-0.1.47.jar 
+set JSCH=C:\tools\libs\jsch-0.1.49\jsch-0.1.49.jar 
 if exist lib goto :compile
 mkdir lib
 :compile
